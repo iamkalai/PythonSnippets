1cbdd512a5fd4945e2cb00ee9a739476c8673d10
Kalaiarasan Pushpanathan <kalaiarasan0215@gmail.com>
2020-04-11 20:19:38 +0530

    Cleaned up lockdown file

--- LockDownCounter.py
+++ LockDownCounter.py
@@ -12,39 +12,43 @@ def incrementdayscounter(filename):
 # read a line from file
 def readfromfile(filename):
     text = ""
-    with open(filename, "r") as file:
-        text = file.readline()
-        file.close()
+    with open(filename, "rt") as file:
+        text = file.read()
     return text
 
 
 # write a line to file
 def writetofile(filename,text):
-    with open(filename, "w") as file:
+    with open(filename, "wt") as file:
         file.write(text)
-        file.close()
+
+
+# tweet and save the id back to file
+def tweetandsaveid(filename, message):
+    tweet_id_to_reply = readfromfile(filename)
+    tweet = replymessage(message, tweet_id_to_reply)
+    writetofile(filename, str(tweet.id))
 
 
 # reply to a tweet
-def replyMessage(message, tweet_id):
+def replymessage(message, tweet_id):
     # add your Twitter app details here
-    CONSUMER_KEY = "add here"
-    CONSUMER_SECRET = "add here"
-    ACCESS_KEY = "add here"
-    ACCESS_SECRET = "add here"
+    consumer_key = ""
+    consumer_secret = ""
+    access_key = ""
+    access_secret = ""
 
     # sets up the authentication
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
 
     # post the tweet and delete the image from disk
     api = tweepy.API(auth)
-    return (api.update_status(message, tweet_id))
+    return api.update_status(message, tweet_id)
 
 
+tweet_id_file = "" # add the file path to be used for storing tweet id
+counter_file = "" # add the file path to be used for storing day counter
+message = "Day "+incrementdayscounter(counter_file)+" without pants."
+tweetandsaveid(tweet_id_file,message)
 
-tweet_id_to_reply = readfromfile("tweetid.txt")
-days = incrementdayscounter("counter.txt")
-message = "Day "+days+" without pants."
-tweet = replyMessage(message,tweet_id_to_reply)
-writetofile("tweetid.txt",str(tweet.id))
